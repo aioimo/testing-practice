143207434fe3bfcee7ad9d3bea13bdb546b5acdd
Anthony Ioimo <anthonylioimo@gmail.com>
2019-01-20 09:41:15 +0100

    starting higher order components section

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import { Route, Link } from 'react-router-dom'
+import CommentBox from 'components/CommentBox'
+import CommentList from 'components/CommentList'
+import { connect } from 'react-redux'
+
+ class App extends Component {
+  renderButton() {
+    if (this.props.auth) {
+      return (
+        <button>Sign Out</button>
+      )
+    } else {
+      return (
+        <button>Sign In</button>
+      )
+    }
+  }
+  renderHeader() {
+    return (
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/post">Post</Link></li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+      {this.renderHeader()}
+      <Route path="/post" component={CommentBox} />
+      <Route path="/" exact component={CommentList} />
+    </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return { auth: state.auth}
+}
+
+export default connect(mapStateToProps)(App);
